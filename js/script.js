21d530cb0c63139b30f20cdcb91ffdc0bc8cb9cf
Aleksandr Zautsev <alexandr.falkone@yandex.ru>
2023-03-20 23:13:03 +0300

    Added slide navigation

--- js/script.js
+++ js/script.js
@@ -412,6 +412,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Слайдер
 
   const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector('.offer__slider'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
     total = document.querySelector('#total'),
@@ -458,6 +459,9 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach((dot) => (dot.style.opacity = '.5'));
+    dots[slideIndex - 1].style.opacity = 1;
   });
 
   prev.addEventListener('click', () => {
@@ -479,44 +483,77 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach((dot) => (dot.style.opacity = '.5'));
+    dots[slideIndex - 1].style.opacity = 1;
   });
 
-  // Простая реализация слайда
+  // Навигация для слайда
 
-  // if (slides.length < 10) {
-  //   total.textContent = `0${slides.length}`;
-  // } else {
-  //   total.textContent = slides.length;
-  // }
+  slider.style.position = 'relative';
+  const indicators = document.createElement('ol');
+  const dots = [];
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+  `;
 
-  // showSlide(slideIndex);
+  slider.append(indicators);
 
-  // function showSlide(n) {
-  //   if (n > slides.length) {
-  //     slideIndex = 1;
-  //   }
-  //   if (n < 1) {
-  //     slideIndex = slides.length;
-  //   }
-  //   slides.forEach((item) => (item.style.display = 'none'));
+  for (i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: 0.5;
+        transition: opacity 0.6s ease;
+    `;
 
-  //   slides[slideIndex - 1].style.display = 'block';
+    indicators.append(dot);
+    dots.push(dot);
 
-  //   if (slides.length < 10) {
-  //     current.textContent = `0${slideIndex}`;
-  //   } else {
-  //     current.textContent = slideIndex;
-  //   }
-  // }
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+  }
 
-  // function plusSlide(n) {
-  //   showSlide((slideIndex += n));
-  // }
+  dots.forEach((dot) => {
+    dot.addEventListener('click', function (e) {
+      const slideTo = e.target.getAttribute('data-slide-to');
 
-  // prev.addEventListener('click', () => {
-  //   plusSlide(-1);
-  // });
-  // next.addEventListener('click', () => {
-  //   plusSlide(1);
-  // });
+      slideIndex = slideTo;
+
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+      sliderInner.style.transform = `translateX(-${offset}px)`;
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach((dot) => (dot.style.opacity = '.5'));
+      dots[slideIndex - 1].style.opacity = 1;
+    });
+  });
 });
